8db51851f25863f148360f2e50d976d94e13641b
tranthi2965 <tranthi2965@gmail.com>
2025-08-17 16:02:40 +0700

    upload file aws s3

---
+++ file-service/src/main/java/com/hoaithi/file_service/controller/FileController.java
@@ -0,0 +1,27 @@
+package com.hoaithi.file_service.controller;
+
+import com.hoaithi.file_service.service.FileStorageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/files")
+public class FileController {
+
+    private final FileStorageService fileStorageService;
+    public FileController(FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String key = fileStorageService.uploadFile(file);
+            String url = fileStorageService.generatePresignedUrl(key);
+            return ResponseEntity.ok(url);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Upload failed: " + e.getMessage());
+        }
+    }
+}
